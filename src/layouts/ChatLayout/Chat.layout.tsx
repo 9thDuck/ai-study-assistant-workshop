985fa6918f55b38da3c4907e3cab9110eb494e67
Mylavarapu Sarath Sai <msai@live.co.uk>
2024-08-29 01:21:18 +0530

    removed container styling because that will come on parent

--- src/layouts/ChatLayout/Chat.layout.tsx
+++ src/layouts/ChatLayout/Chat.layout.tsx
@@ -13,9 +13,7 @@ export const ChatLayout: React.FC<ChatLayoutProps> = ({
 }) => {
   return (
     <div className={clsx('h-screen overflow-hidden', className)} {...props}>
-      <div
-        className={clsx('h-full container mx-auto flex flex-col', className)}
-      >
+      <div className={clsx('h-full w-full flex flex-col', className)}>
         <div className="flex-grow flex-shrink-0 pt-[40px] overflow-hidden flex flex-col">
           <div className="mt-1"></div>
           <div className="overflow-auto overflow-x-hidden flex-grow h-0 pe-2">
